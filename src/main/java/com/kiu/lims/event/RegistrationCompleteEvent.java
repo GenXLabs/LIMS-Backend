a5115b25fd667e66c0965865d39390c1a74950aa
it21256264 <it21256264@my.sliit.lk>
2023-09-06 20:35:41 +0530

    Create User.java
    Update application.properties
    
    add lambok dependency
    
    Create User.java
    
    Delete User.java
    
    Create RegistrationCompleteEventListener.java
    
    Create RegistrationCompleteEvent.java
    
    Create UserAlreadyExistsException.java
    
    Create RegistrationController.java
    
    Create RegistrationRequest.java
    
    Create UserRegistrationSecurityConfig.java
    
    Create IUserService.java
    
    Create UserController.java
    
    Create UserRepository.java
    
    Create UserService.java

---
+++ src/main/java/com/kiu/lims/event/RegistrationCompleteEvent.java
@@ -0,0 +1,19 @@
+package com.kiu.lims.event;
+
+import com.kiu.lims.user.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+@Setter
+public class RegistrationCompleteEvent extends ApplicationEvent {
+    private User user;
+    private String applicationUrl;
+
+    public RegistrationCompleteEvent(User user, String applicationUrl) {
+        super(user);
+        this.user = user;
+        this.applicationUrl = applicationUrl;
+    }
+}
