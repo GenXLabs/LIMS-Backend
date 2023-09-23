f41d4818b5c6f8c47041b83f923d526dd25cc39a
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2023-09-23 23:30:42 +0530

    update

--- src/main/java/com/kiu/lims/controller/LabReservationController.java
+++ src/main/java/com/kiu/lims/controller/LabReservationController.java
@@ -3,11 +3,13 @@ package com.kiu.lims.controller;
 import com.kiu.lims.model.ResponseModel;
 import com.kiu.lims.service.LabReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/lims/lab-reservation")
 public class LabReservationController {
 
--- src/main/java/com/kiu/lims/entity/LabReservationEntity.java
+++ src/main/java/com/kiu/lims/entity/LabReservationEntity.java
@@ -1,13 +1,12 @@
 package com.kiu.lims.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Entity
 @Data
@@ -19,6 +18,7 @@ public class LabReservationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reservation_id", unique = true)
+    @JsonProperty("reservation_id")
     private Long reservationId;
 
     private String title;
@@ -29,27 +29,36 @@ public class LabReservationEntity {
 
     private String date;
 
+    @JsonProperty("start_time")
     private String startTime;
 
+    @JsonProperty("end_time")
     private String endTime;
 
     private String description;
 
     private String calendar; // Business, Personal, etc.
 
+    @JsonProperty("requester_id")
     private Long requesterId;
 
     private Byte status;
 
+    @JsonProperty("created_by")
     private Integer createdBy;
 
+    @JsonProperty("updated_by")
     private Integer updatedBy;
 
+    @JsonProperty("deleted_by")
     private Integer deletedBy;
 
+    @JsonProperty("created_at")
     private Timestamp createdAt;
 
+    @JsonProperty("updated_at")
     private Timestamp updatedAt;
 
+    @JsonProperty("deleted_at")
     private Timestamp deletedAt;
 }
