c4980548f72a0b2323981bd2360d8e5fa6f7f3b4
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2023-09-25 14:47:18 +0530

    update

--- src/main/java/com/kiu/lims/entity/EmailRequest.java
+++ src/main/java/com/kiu/lims/entity/EmailRequest.java
@@ -1,47 +1,30 @@
 package com.kiu.lims.entity;
 
 public class EmailRequest {
-    private String from;
-    private String to;
+    private String recipient;
     private String subject;
     private String content;
 
-    // Getter for 'from' field
-    public String getFrom() {
-        return from;
+    public String getRecipient() {
+        return recipient;
     }
 
-    // Setter for 'from' field
-    public void setFrom(String from) {
-        this.from = from;
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
     }
 
-    // Getter for 'to' field
-    public String getTo() {
-        return to;
-    }
-
-    // Setter for 'to' field
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    // Getter for 'subject' field
     public String getSubject() {
         return subject;
     }
 
-    // Setter for 'subject' field
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
-    // Getter for 'content' field
     public String getContent() {
         return content;
     }
 
-    // Setter for 'content' field
     public void setContent(String content) {
         this.content = content;
     }
