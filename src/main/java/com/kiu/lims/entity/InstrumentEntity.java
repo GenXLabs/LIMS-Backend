6f1b24b21361b14890e897d84afa075efd54da8b
dilshanravindu <dilshanravindufd@gmail.com>
2023-10-04 21:06:31 +0530

    sop
    new

--- src/main/java/com/kiu/lims/entity/InstrumentEntity.java
+++ src/main/java/com/kiu/lims/entity/InstrumentEntity.java
@@ -40,4 +40,6 @@ public class InstrumentEntity {
 
     @JsonProperty("deleted_at")
     private Timestamp deletedAt;
+
+
 }
--- src/main/java/com/kiu/lims/service/InstrumentService.java
+++ src/main/java/com/kiu/lims/service/InstrumentService.java
@@ -2,10 +2,13 @@ package com.kiu.lims.service;
 
 import com.kiu.lims.entity.InstrumentEntity;
 import com.kiu.lims.model.ResponseModel;
+import org.springframework.core.io.Resource;
 
 public interface InstrumentService {
     ResponseModel getAllSopManagement();
     ResponseModel createSop(InstrumentEntity newSop);
     ResponseModel updateSop(Long sopId, InstrumentEntity updatedSop);
     ResponseModel deleteSop(Long sopId);
+
+
 }
